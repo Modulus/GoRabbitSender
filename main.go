b543a90e20dce3e71c535141f9e4494974da7368
John Sigvald Skauge <thejohnskauge@gmail.com>
2018-07-12 18:29:58 +0200

    Updated

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
+
+	"github.com/olivere/elastic"
 )
 
 type Message struct {
@@ -12,22 +15,22 @@ func main() {
 	message := CreateQuote()
 	log.Printf("Message: %s", message)
 
-	// ctx := context.Background()
-	// client, err := elastic.NewClient()
-	// if err != nil {
-	// 	// Handle error
-	// }
-	// message := Message{Message: "Take Five"}
-	// _, err = client.Index().
-	// 	Index("message").
-	// 	Type("doc").
-	// 	//Id("1").
-	// 	BodyJson(message).
-	// 	Refresh("wait_for").
-	// 	Do(ctx)
-	// if err != nil {
-	// 	// Handle error
-	// 	panic(err)
-	// }
+	ctx := context.Background()
+	client, err := elastic.NewClient()
+	if err != nil {
+		// Handle error
+	}
+	message := Message{Message: "Take Five"}
+	_, err = client.Index().
+		Index("message").
+		Type("doc").
+		//Id("1").
+		BodyJson(message).
+		Refresh("wait_for").
+		Do(ctx)
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
 
 }
